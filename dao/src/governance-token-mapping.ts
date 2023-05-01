00e3e7214c80e902d0cf992b269d799545843a38
Stefano Ceschi Berrini <stefano.ceschib@gmail.com>
2023-05-01 12:29:48 +0200

    refactor: added abstract transfer fn

--- dao/src/governance-token-mapping.ts
+++ dao/src/governance-token-mapping.ts
@@ -1,113 +1,32 @@
 import { Address, log } from "@graphprotocol/graph-ts";
 import {
-  GovernanceToken,
   Transfer,
   VestingSet,
 } from "../generated/GovernanceToken/GovernanceToken";
 import { getDaoUser } from "./dao-user";
-import { Voting } from "../generated/Voting/Voting";
-import {
-  GOVERNANCE_TOKEN_CONTRACT_ADDRESS,
-  INTERNAL_MARKET_CONTRACT_ADDRESS,
-  VOTING_CONTRACT_ADDRESS,
-} from "../generated/addresses";
-import { InternalMarket } from "../generated/InternalMarket/InternalMarket";
-import { Offer } from "../generated/schema";
-import { getDaoManagerEntity } from "./dao-manager";
 
-const saveDaoUserData = (
-  votingContract: Voting,
-  internalMarketContract: InternalMarket,
-  governanceTokenContract: GovernanceToken,
-  event: Transfer,
-  userAddress: Address
-): void => {
-  log.info("Save dao user data for address {}", [userAddress.toHexString()]);
-  if (userAddress != Address.zero()) {
-    const daoUser = getDaoUser(userAddress.toHexString());
-    daoUser.address = userAddress;
-    daoUser.votingPower = votingContract.getVotingPower(userAddress);
-    const governanceWithdrawableTempBalance = internalMarketContract.withdrawableBalanceOf(
-      userAddress
-    );
-    const governanceOfferedTempBalance = internalMarketContract.offeredBalanceOf(
-      userAddress
-    );
-    daoUser.governanceWithdrawableTempBalance = governanceWithdrawableTempBalance;
-    log.info("governanceWithdrawableTempBalance {}", [
-      governanceWithdrawableTempBalance.toHexString(),
-    ]);
-    daoUser.governanceOfferedTempBalance = governanceOfferedTempBalance;
-    daoUser.governanceVaultedBalance = governanceWithdrawableTempBalance.plus(
-      governanceOfferedTempBalance
-    );
-    daoUser.governanceBalance = governanceTokenContract.balanceOf(userAddress);
-    daoUser.governanceVestingBalance = governanceTokenContract.vestingBalanceOf(
-      userAddress
-    );
-    const newActiveOffers: string[] = [];
-    for (let index = 0; index < daoUser.activeOffers.length; index++) {
-      const offerId = daoUser.activeOffers[index];
-      const offer = Offer.load(offerId);
-      if (offer && offer.expirationTimestamp >= event.block.timestamp) {
-        log.info(
-          "offer non expired {} offer expiration {} event block timestamp {}",
-          [
-            offerId,
-            offer.expirationTimestamp.toString(),
-            event.block.timestamp.toString(),
-          ]
-        );
-        newActiveOffers.push(offer.id);
-      }
-      if (offer && offer.expirationTimestamp < event.block.timestamp) {
-        log.info(
-          "offer expired {} offer expiration {} event block timestamp {}",
-          [
-            offerId,
-            offer.expirationTimestamp.toString(),
-            event.block.timestamp.toString(),
-          ]
-        );
-        offer.expiredOnTransfer = true;
-        offer.save();
-      }
-    }
-    daoUser.activeOffers = newActiveOffers;
-    daoUser.save();
-  }
-};
+import { getDaoManagerEntity } from "./dao-manager";
+import saveDaoUserData from "./save-dao-user-data";
+import { VOTING_CONTRACT_ADDRESS } from "../generated/addresses";
+import { Voting } from "../generated/Voting/Voting";
 
 export function handleTransfer(event: Transfer): void {
   const addressTo = event.params.to;
   const addressFrom = event.params.from;
+  const value = event.params.value;
+
+  log.info("handleTransfer called addressFrom: {}, addressTo: {}, value: {}", [
+    addressFrom.toHexString(),
+    addressTo.toHexString(),
+    value.toString(),
+  ]);
+
+  saveDaoUserData(addressFrom, event.block.timestamp);
+
+  saveDaoUserData(addressTo, event.block.timestamp);
 
   const votingContract = Voting.bind(
     Address.fromString(VOTING_CONTRACT_ADDRESS)
-  );
-
-  const internalMarketContract = InternalMarket.bind(
-    Address.fromString(INTERNAL_MARKET_CONTRACT_ADDRESS)
-  );
-
-  const governanceTokenContract = GovernanceToken.bind(
-    Address.fromString(GOVERNANCE_TOKEN_CONTRACT_ADDRESS)
-  );
-
-  saveDaoUserData(
-    votingContract,
-    internalMarketContract,
-    governanceTokenContract,
-    event,
-    addressFrom
-  );
-
-  saveDaoUserData(
-    votingContract,
-    internalMarketContract,
-    governanceTokenContract,
-    event,
-    addressTo
   );
 
   const daoManagerEntity = getDaoManagerEntity();
--- dao/src/neokingdom-token-mapping.ts
+++ dao/src/neokingdom-token-mapping.ts
@@ -1,39 +1,10 @@
-import { Address } from "@graphprotocol/graph-ts";
-import {
-  NeokingdomToken,
-  Transfer,
-} from "../generated/NeokingdomToken/NeokingdomToken";
-import { NEOKINGDOM_TOKEN_CONTRACT_ADDRESS } from "../generated/addresses";
-import { getDaoUser } from "./dao-user";
+import { Transfer } from "../generated/NeokingdomToken/NeokingdomToken";
+import saveDaoUserData from "./save-dao-user-data";
 
 export function handleTransfer(event: Transfer): void {
   const addressTo = event.params.to;
   const addressFrom = event.params.from;
 
-  const addressToHex = event.params.to.toHexString();
-  const addressFromHex = event.params.from.toHexString();
-
-  const nekoingdomTokenContract = NeokingdomToken.bind(
-    Address.fromString(NEOKINGDOM_TOKEN_CONTRACT_ADDRESS)
-  );
-
-  if (addressFrom != Address.zero()) {
-    const daoUserFrom = getDaoUser(addressFromHex);
-    daoUserFrom.address = addressFrom;
-    daoUserFrom.neokigdomTokenBalance = nekoingdomTokenContract.balanceOf(
-      addressFrom
-    );
-
-    daoUserFrom.save();
-  }
-
-  if (addressTo != Address.zero()) {
-    const daoUserTo = getDaoUser(addressToHex);
-    daoUserTo.address = addressTo;
-    daoUserTo.neokigdomTokenBalance = nekoingdomTokenContract.balanceOf(
-      addressTo
-    );
-
-    daoUserTo.save();
-  }
+  saveDaoUserData(addressFrom, event.block.timestamp);
+  saveDaoUserData(addressTo, event.block.timestamp);
 }
