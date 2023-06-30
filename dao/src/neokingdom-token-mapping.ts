54d9a857c6ce88a24986f70e15d5d9a2406b6a43
Stefano Ceschi Berrini <stefano.ceschib@gmail.com>
2023-06-30 14:17:45 +0200

    feat: new tokenMinting

--- dao/src/neokingdom-token-mapping.ts
+++ dao/src/neokingdom-token-mapping.ts
@@ -1,5 +1,7 @@
+import { Address, log } from "@graphprotocol/graph-ts";
 import { Transfer } from "../generated/NeokingdomToken/NeokingdomToken";
 import saveDaoUserData from "./save-dao-user-data";
+import { TokenMinting } from "../generated/schema";
 
 export function handleTransfer(event: Transfer): void {
   const addressTo = event.params.to;
@@ -7,4 +9,26 @@ export function handleTransfer(event: Transfer): void {
 
   saveDaoUserData(addressFrom, event.block);
   saveDaoUserData(addressTo, event.block);
+
+  if (event.params.from == Address.zero()) {
+    const blockHash = event.block.hash.toHexString();
+
+    // we should also create a new TokenMinting entity with:
+    const tokenMinting =
+      TokenMinting.load(blockHash) || new TokenMinting(blockHash);
+
+    if (!tokenMinting) {
+      log.critical("TokenMinting entity {} is null", [blockHash]);
+      return;
+    }
+
+    if (!tokenMinting.amount) {
+      tokenMinting.amount = [];
+    }
+
+    tokenMinting.amount = tokenMinting.amount.concat([event.params.value]);
+    tokenMinting.mintedTimestamp = event.block.timestamp;
+
+    tokenMinting.save();
+  }
 }
