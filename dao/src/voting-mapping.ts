1f9f6caedfdc023df20eebe18d435b16aa1b439d
Stefano Ceschi Berrini <stefano.ceschib@gmail.com>
2023-10-03 11:06:32 +0200

    fix: save user's data after delegating

--- dao/src/voting-mapping.ts
+++ dao/src/voting-mapping.ts
@@ -1,6 +1,7 @@
 import { DelegateChanged } from "../generated/Voting/Voting";
 import { DelegationUser } from "../generated/schema";
 import { log } from "@graphprotocol/graph-ts";
+import saveDaoUserData from "./save-dao-user-data";
 
 export function handleDelegateChanged(event: DelegateChanged): void {
   const delegatorHexString = event.params.delegator.toHexString();
@@ -19,5 +20,13 @@ export function handleDelegateChanged(event: DelegateChanged): void {
     currentDelegator.address = event.params.delegator;
     currentDelegator.delegated = event.params.newDelegate;
     currentDelegator.save();
+  }
+
+  saveDaoUserData(event.params.delegator, event.block);
+
+  // if the delegator is delegating to someone else, we need to update the
+  // new delegate's data as well
+  if (event.params.delegator != event.params.newDelegate) {
+    saveDaoUserData(event.params.newDelegate, event.block);
   }
 }
