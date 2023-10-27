ce7304af8dc7b991aebd7c1ec501d02290160ca6
Stefano Ceschi Berrini <stefano.ceschib@gmail.com>
2023-10-27 16:40:05 +0200

    fix: parameters for redemption

--- dao/src/redemption-controller-mapping.ts
+++ dao/src/redemption-controller-mapping.ts
@@ -3,29 +3,29 @@ import { Redemption, RedemptionHistory } from "../generated/schema";
 // see https://excalidraw.com/#room=086049ebc8e9dc8d6ba3,TNTY6gcjMRZP1XacEpfUgg
 
 export function handleRedemptionCreated(event) {
-  const position = event.params.position.toString();
+  const index = event.params.index.toString();
   const from = event.params.from;
-  const redemption = new Redemption(position + "-" + from.toHexString());
+  const redemption = new Redemption(index + "-" + from.toHexString());
 
   redemption.createBy = from;
   redemption.amount = event.params.amount;
-  redemption.startTimestamp = event.params.startTimestamp;
-  redemption.endTimestamp = event.params.endTimestamp;
+  redemption.startTimestamp = event.params.starts;
+  redemption.endTimestamp = event.params.ends;
 
   redemption.redemptionHistory = [];
   redemption.save();
 }
 
 export function handleRedemptionUpdated(event) {
-  const position = event.params.position.toString();
+  const index = event.params.index.toString();
   const from = event.params.from;
-  const redemptionId = position + "-" + from.toHexString();
+  const redemptionId = index + "-" + from.toHexString();
   const redemption = Redemption.load(redemptionId);
 
   if (redemption) {
     const historyItemsLength = redemption.redemptionHistory.length;
     const redemptionHistoryItem = new RedemptionHistory(redemptionId + "-" + (historyItemsLength + 1));
-    redemptionHistoryItem.amount = event.params.amount;
+    redemptionHistoryItem.amount = event.params.amountRedeemed;
     redemptionHistoryItem.timestamp = event.block.timestamp;
     redemptionHistoryItem.save();
 
